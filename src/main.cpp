d6346562355ca72136764c0c0cdc25fbe3957c22
sustainablelab <sustainablelab@gmail.com>
2022-11-25 01:25:31 -0500

    Put artwork on a texture for alpha blending

--- src/main.cpp
+++ src/main.cpp
@@ -42,9 +42,11 @@ WindowInfo::WindowInfo(int argc, char* argv[])
 
 SDL_Window* win;
 SDL_Renderer* ren;
+SDL_Texture *art_tex;                                   // Draw artwork on this texture
 
 void shutdown(void)
 {
+    SDL_DestroyTexture(art_tex);
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
     SDL_Quit();
@@ -62,10 +64,28 @@ int main(int argc, char* argv[])
         ren_flags |= SDL_RENDERER_PRESENTVSYNC;         // 60 fps! No SDL_Delay()
         ren_flags |= SDL_RENDERER_ACCELERATED;          // Hardware acceleration
         ren = SDL_CreateRenderer(win, -1, ren_flags);
+
+        // Set up transparency blending for a transparent heads-up overlay.
+        // Just this line is enough to start using the alpha channel on my overlay.
+        SDL_SetRenderDrawBlendMode(ren, SDL_BLENDMODE_BLEND); // Draw with alpha
+        // But that only mixes it with the background.
+        // So I draw artwork to texture art_tex:
+        art_tex = SDL_CreateTexture(ren,
+                SDL_PIXELFORMAT_RGBA8888,
+                SDL_TEXTUREACCESS_TARGET,               // Use art_tex as render target
+                wI.w, wI.h);                            // Match the window size
+        // And I set alpha blending for my artwork texture:
+        if(SDL_SetTextureBlendMode(art_tex, SDL_BLENDMODE_BLEND) < 0)
+        { // Texture blending is not supported, exit program.
+            puts("Cannot set texture to blendmode blend.");
+            shutdown();
+            return EXIT_FAILURE;
+        }
     }
 
-    int bgnd_color=0;                                   // Index into Colors::list
+    int bgnd_color=6;                                   // Index into Colors::list
     int fgnd_color=Colors::contrasts(bgnd_color);       // Pick a nice foreground
+    bool show_overlay;                                  // Help on/off
     bool quit = false;
     while (!quit)
     {
@@ -87,6 +107,13 @@ int main(int argc, char* argv[])
 
                     case SDL_WINDOWEVENT_RESIZED:
                         SDL_GetWindowSize(win, &(wI.w), &(wI.h));
+                        // Resize texture for alpha blending artwork
+                        SDL_DestroyTexture(art_tex);
+                        art_tex = SDL_CreateTexture(ren,
+                                SDL_PIXELFORMAT_RGBA8888,
+                                SDL_TEXTUREACCESS_TARGET,
+                                wI.w, wI.h);
+                        SDL_SetTextureBlendMode(art_tex, SDL_BLENDMODE_BLEND);
                         break;
 
                     default: break;
@@ -106,6 +133,10 @@ int main(int argc, char* argv[])
                         if(  kmod&KMOD_SHIFT  ) Colors::prev(bgnd_color);
                         else                    Colors::next(bgnd_color);
                         fgnd_color = Colors::contrasts(bgnd_color);
+                        break;
+
+                    case SDLK_SLASH:                    // ? : Toggle help
+                        if(  kmod&KMOD_SHIFT  ) show_overlay = !show_overlay;
                         break;
 
                     default: break;
@@ -128,8 +159,28 @@ int main(int argc, char* argv[])
             float M = 0.01*W;                     // M : Margin in pixels
             SDL_FRect border = {.x=M, .y=M, .w=W-2*M, .h=H-2*M};
             SDL_Color c = Colors::list[fgnd_color];
+            // Render
+            SDL_SetRenderTarget(ren, art_tex);          // Render artwork to texture for alpha blending
+            SDL_SetRenderDrawColor(ren, 0,0,0,0);       // Start with blank texture
+            SDL_RenderClear(ren);                       // Blank the texture
             SDL_SetRenderDrawColor(ren, c.r, c.g, c.b, c.a);
             SDL_RenderDrawRectF(ren, &border);
+            SDL_SetRenderTarget(ren, NULL);
+            SDL_Rect srcrect = {.x=0,.y=0,.w=wI.w,.h=wI.h};
+            SDL_RenderCopy(ren, art_tex, &srcrect,&srcrect);
+        }
+        if(  show_overlay  )
+        { // Overlay help
+            SDL_Color c = Colors::coal;
+            SDL_SetRenderDrawColor(ren, c.r, c.g, c.b, c.a>>1); // Black 50% opacity
+            if(  bgnd_color == Colors::COAL  )
+            {
+                c = Colors::snow;
+                SDL_SetRenderDrawColor(ren, c.r, c.g, c.b, c.a>>3); // White 12% opacity
+            }
+
+            SDL_Rect rect = {.x=0, .y=0, .w=wI.w, .h=100};
+            SDL_RenderFillRect(ren, &rect);             // Draw filled rect
         }
         SDL_RenderPresent(ren);
     }
